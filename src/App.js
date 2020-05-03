9f6f08488c9a01b1ef06fb31ffbb274daec7eb05
Romulo Martins da Silva <romulo.martins@olxbr.com>
2020-05-03 12:01:19 -0300

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+import styles from './style';
+
+function App() {
+  const emptyBoard = Array(9).fill('');
+  const [board, setBoard] = useState(emptyBoard);
+  const [currentPlayer, setCurrentPlayer] = useState('X');
+  const [winner, setWinner] = useState(null);
+
+  function handleWithClick(index) {
+    if (winner) {
+      console.log('Jogo Finalizado!')
+      return;
+    }
+
+    if (board[index] !== '') {
+      console.log('Posicao Ocupada!')
+      return;
+    }
+
+    setBoard(
+      board.map((item, itemIndex) => (
+        item = index === itemIndex ? currentPlayer : item
+      ))
+    );
+
+    setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+  }
+
+  function checkWinner() {
+    const possibleWaysToWin = [
+      [board[0], board[1], board[2]],
+      [board[3], board[4], board[5]],
+      [board[6], board[7], board[8]],
+
+      [board[0], board[3], board[6]],
+      [board[1], board[4], board[7]],
+      [board[2], board[5], board[8]],
+
+      [board[6], board[4], board[2]],
+      [board[0], board[4], board[8]]
+    ]
+
+    possibleWaysToWin.forEach(cells => {
+      if (cells.every(cell => cell === 'X')) setWinner('X');
+      if (cells.every(cell => cell === 'O')) setWinner('O');
+    })
+
+    if (board.every(cell => cell !== '')) {
+      setWinner('E')
+    }
+  }
+
+  function restartGame() {
+    setBoard(emptyBoard)
+    setCurrentPlayer('O')
+    setWinner(null)
+  }
+
+  function getCellStyle(item) {
+    switch (item) {
+      case 'X':
+        return styles.cellX;
+      case 'O':
+        return styles.cellO;
+      default:
+        return null;
+    }
+  }
+
+  useEffect(checkWinner);
+
+  return (
+    <SafeAreaView style={styles.body}>
+      <Text style={styles.title}>Jogo da Velha</Text>
+      <View style={[styles.board, winner ? styles.gameOver : null ]}>
+        {board.map((item, index) => (
+          <TouchableOpacity
+            key={index}
+            style={[styles.cell, getCellStyle(item)]}
+            onPress={() => handleWithClick(index)}>
+            <Text style={styles.cellText}>{item}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      {winner &&
+        <View style={styles.footer}>
+          {winner === 'E' ?
+            <View style={styles.winner}>
+              <Text>Empate!</Text>
+            </View>
+            :
+            <View style={styles.winner}>
+              <Text>{`O jogador ${winner} venceu!`}</Text>
+            </View>
+          }
+          <TouchableOpacity
+            onPress={restartGame}
+            style={styles.restartButton}>
+            <Text style={styles.textButton}>Recomecar Jogo</Text>
+          </TouchableOpacity>
+        </View>
+      }
+    </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ src/style.js
@@ -0,0 +1,91 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+const styles = StyleSheet.create({
+    body: {
+      flex: 1,
+      alignItems: 'center',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontFamily: 'Helvetica, sans-serif',
+      backgroundColor: '#f2f2f2',
+    },
+    board: {
+      flex: 1,
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      alignItems: 'center',
+      justifyContent: 'center',
+      alignContent: 'center',
+    },
+    title: {
+      fontSize: 32,
+      fontWeight: '600',
+      textAlign: 'center',
+      color: '#595959',
+    },
+    cell: {
+      width: (Dimensions.get('window').width - 50) / 3,
+      height: (Dimensions.get('window').width - 50) / 3,
+      backgroundColor: 'white',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 5,
+      margin: 5,
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 5,
+    },
+    cellText: {
+      fontSize: 30,
+      fontWeight: 'bold',
+      color: '#404040',
+    },
+    cellX: {
+      backgroundColor: '#ff5050',
+    },
+    cellO: {
+      backgroundColor: '#3399ff',
+    },
+    winner: {
+      textAlign: 'center',
+      fontSize: 1.5,
+      margin: 5,
+    },
+    restartButton: {
+      width: 150,
+      height: 50,
+      borderRadius: 5,
+      backgroundColor: '#40bf40',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontWeight: 'bold',
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 5,
+      elevation: 5,
+      margin: 10,
+    },
+    textButton: {
+      color: 'white',
+      fontWeight: 'bold',
+    },
+    footer: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      textShadowColor: '#000',
+    },
+    gameOver: {
+      opacity: 0.5,
+    },
+  });
+
+  export default styles;
